cd9a3ac26203b89ece8847232c2f198607bebddd
Chris <109135142+valentim-debora@users.noreply.github.com>
2024-07-16 08:39:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ''
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        console.log(consultaCEPConvertida);
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = '<p>CEP inválido. Tente novamente.</p>'
+        console.log(erro);
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
